9481ce470067d9ba8c38d8aa11047da6cba54fd3
Rodrigo Llull Torres <rllullt@gmail.com>
2019-09-11 14:10:56 -0300

    Cambios para poder generar un dataset de todos los años para las ETAs

--- src/cargar_etas.r
+++ src/cargar_etas.r
@@ -1,6 +1,27 @@
 # Cargar los datos de Enfermedades Transmitidas por alimentos
-library(xlsx)
+#library(xlsx)
+
+## TODO
+# Guardar todos los excel como CSV, luego cargarlos con read.csv, y finalmente hacer rbind entre todas
+# las tablas generadas.
+
+library(readxl)
 directorio_padre <- "../Datasets brotes ETA/"
+
+generarEtas <- function() {
+  eta2011 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2011, ".xlsx", sep=""))
+  eta2012 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2012, ".xlsx", sep=""))
+  eta2013 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2013, ".xlsx", sep=""))
+  eta2014 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2014, ".xlsx", sep=""))
+  eta2015 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2015, ".xlsx", sep=""))
+  eta2016 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2016, ".xlsx", sep=""))
+  eta2017 <- read_xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2017, ".xlsx", sep=""))
+  (etas <- rbind(eta2011, eta2012, eta2013, eta2014, eta2015, eta2016, eta2017))
+}
+
+etas <- generarEtas()
+
+if (FALSE) { # forma antigua de hacerlo
 eta2011 <- read.xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2011, ".xlsx", sep=""),
                      sheetIndex=1, header=TRUE)
 eta2012 <- read.xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2012, ".xlsx", sep=""),
@@ -15,3 +36,4 @@ eta2016 <- read.xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2016, ".xls
                      sheetIndex=1, header=TRUE)
 eta2017 <- read.xlsx(paste(directorio_padre, "Bases de brotes ETA ", 2017, ".xlsx", sep=""),
                      sheetIndex=1, header=TRUE)
+}
